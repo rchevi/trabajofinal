ec34e6bceaad81cd8d6c2211cb36bbdc4049bfba
unknown <rchevi@hotmail.com>
2024-03-24 18:45:42 -0300

    primera subida a git

---
+++ script.js
@@ -0,0 +1,29 @@
+document.querySelectorAll('.piezas img').forEach(img => {
+    img.addEventListener('dragstart', function(e) {
+        e.dataTransfer.setData('text/plain', e.target.src);
+    });
+})
+
+document.querySelectorAll('.solucion').forEach(solucion => {
+    solucion.addEventListener('dragover', function(e) {
+        e.preventDefault();
+    });
+
+    solucion.addEventListener('drop', function(e) {
+        e.preventDefault();
+        const imgSrc = e.dataTransfer.getData('text/plain');
+        const img = document.createElement('img');
+        img.src = imgSrc;
+        img.draggable = true;
+        solucion.appendChild(img);
+    });
+});
+
+document.getElementById('reiniciar').addEventListener('click', function() {
+    document.querySelectorAll('.solucion').forEach(solucion => {
+        while (solucion.firstChild) {
+            solucion.removeChild(solucion.firstChild);
+        }
+    });
+});
+
