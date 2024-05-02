59cb517959c70217d88d3e81938e9cba94588554
Sebastian Ravenna <rchevi@hotmail.com>
2024-05-01 23:06:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -66,35 +66,3 @@ function pad(number) {
         return number;
     }
 }
-
-
-
-
-/*let video=document.querySelector('video');
-let playBoton = document.getElementById("play");
-let pauseBoton= document.getElementById("pause");
-
-
-
-setTimeout(()=>{
-
-    document.getElementById('tiempo').innerHTML=` Duracion video  4:41`},100);
-
-for (let i = 0; i < video.tim; i++) {
-    console.log(video.currentTime)
-    
-}
-
-playBoton.addEventListener('click',()=>{
-    video.play()
-
-   
-});
-
-pauseBoton.addEventListener('click',()=>{
-    video.pause();
-    flag=false;
-});*/
-
-    
-
